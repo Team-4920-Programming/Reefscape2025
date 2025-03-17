a547e27ccac7ba96036c8c03b8ef27cf2b266e22
Anmol <anmol45@gmail.com>
2025-03-16 23:46:28 -0400

    random sim stuff... might be able to fix pathplanner

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -39,11 +39,11 @@ import edu.wpi.first.math.numbers.N3;
  */
 public final class Constants
 {
-  public static final double ROBOT_MOI = 4.7947747641;
-  public static final double ROBOT_MASS = (115) * 0.453592; // 32lbs * kg per pound
+  public static final double ROBOT_MOI = 4.7947748864;
+  public static final double ROBOT_MASS = 68;//(115) * 0.453592; // 32lbs * kg per pound
   public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
   public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
-  public static final double MAX_SPEED  = Units.feetToMeters(14.63);
+  public static final double MAX_SPEED  = 4.0;//Units.feetToMeters(14.63);
   // Maximum speed of the robot in meters per second, used to limit acceleration.
 
 //  public static final class AutonConstants
